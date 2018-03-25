61d55e86a61b2d71b3d8ebcdcb60d1892a63fb90
João Nuno Ferreira <up201605330@g.uporto.pt>
2018-03-25 20:07:05 +0100

    Update Guard.java

--- Projectyoloyo/src/dkeep/logic/Guard.java
+++ Projectyoloyo/src/dkeep/logic/Guard.java
@@ -1,26 +1,55 @@
 package dkeep.logic;
 
-public class Guard extends Character{
+import java.util.Random;
 
-	private int pathStep = 0;
-	private char[] guardpath = {'a','s','s','s','s','a','a','a','a','a','a','s','d','d','d','d','d','d','d','w','w','w','w','w'};
+public class Guard extends Character {
 
-	
+	protected int pathStep = 0;
+	protected char[] guardpath = { 'a', 's', 's', 's', 's', 'a', 'a', 'a', 'a', 'a', 'a', 's', 'd', 'd', 'd', 'd', 'd',
+			'd', 'd', 'w', 'w', 'w', 'w', 'w' };
+	protected boolean asleep;
+	protected boolean reverse;
+	Random r;
+
 	public Guard(int x, int y, char symbol) {
 		super(x, y, symbol);
+		asleep = false;
+		reverse = false;
 	}
-	
-	public void upPathStep() {
-		if(pathStep==guardpath.length-1)
+
+	public void upPathSteps() {
+
+		if (pathStep == guardpath.length - 1)
 			pathStep = 0;
 		else
-		pathStep++;
+			pathStep++;
 	}
+	
+	public void downPathSteps() {
 
-	public void moveGuard(char[][] map) {
-
-		moveCharacter(guardpath[pathStep], map);
-		upPathStep();
+		if (pathStep == 0)
+			pathStep = guardpath.length - 1;
+		else
+			pathStep--;
 	}
+	
+	public void setReverse() {
 
+		if (reverse)
+			reverse = false;
+		else
+			reverse = true;
+	}
+	
+	public char inverseMovement(char mov) {
+
+		if (mov == 'a')
+			return 'd';
+		else if (mov == 'd')
+			return 'a';
+		else if (mov == 'w')
+			return 's';
+		else
+			return 'w';
+	}	
 }
